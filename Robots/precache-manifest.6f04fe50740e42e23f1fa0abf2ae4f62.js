0563f6390f98ab8d829a9708cf8b9e778358732d
Anas Younso <anas@Anass-MBP.neoscope.local>
2019-01-24 16:33:12 +0100

    adding Robot pages

---
+++ Robots/precache-manifest.6f04fe50740e42e23f1fa0abf2ae4f62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/static/media/SEGA.82e09f78.TTF"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cddd93c362f3da6ffdcf",
+    "url": "/static/js/main.cddd93c3.chunk.js"
+  },
+  {
+    "revision": "a7755df4b6840399a9e0",
+    "url": "/static/js/1.a7755df4.chunk.js"
+  },
+  {
+    "revision": "cddd93c362f3da6ffdcf",
+    "url": "/static/css/main.9bd2552a.chunk.css"
+  },
+  {
+    "revision": "a7755df4b6840399a9e0",
+    "url": "/static/css/1.5a5667c3.chunk.css"
+  },
+  {
+    "revision": "2d51e0690612925976dd7b8b1b3ea023",
+    "url": "/index.html"
+  }
+];
