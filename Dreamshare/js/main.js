1753e8130101e7e70b32a4a15f4457b55e2d023a
Anas Younso <anas_younso@hotmail.com>
2019-01-24 10:04:55 +0100

    Update main.js

--- Dreamshare/js/main.js
+++ Dreamshare/js/main.js
@@ -1,8 +1,12 @@
-window.addEventListener('scroll', function() {
+window.onscroll = function() {
+  myFunction();
+};
+
+function myFunction() {
   const header = document.querySelector('.header');
-  if (pageYOffset >= 150) {
+  if (document.body.scrollTop > 150) {
     header.style.backgroundColor = 'rgba(143, 76, 76, 0.527)';
   } else {
-    header.style.backgroundColor = 'none';
+    header.style.backgroundColor = 'rgba(143, 76, 76, 0)';
   }
-});
+}
