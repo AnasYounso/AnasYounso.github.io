5777064627d9b99a3c9607ed0803075b1d4f4036
Anas Younso <anas@Anass-MBP.neoscope.local>
2019-01-30 16:34:34 +0100

    adding Todo List

---
+++ Todo List/precache-manifest.665d426d49bb20a815e1bebc9ad85630.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee9b8c9dba10bf851c49",
+    "url": "/static/js/main.ee9b8c9d.chunk.js"
+  },
+  {
+    "revision": "2a21890cafb1b405f98e",
+    "url": "/static/js/1.2a21890c.chunk.js"
+  },
+  {
+    "revision": "ee9b8c9dba10bf851c49",
+    "url": "/static/css/main.28792191.chunk.css"
+  },
+  {
+    "revision": "cf710743661da8e85556ff9fffcf7149",
+    "url": "/index.html"
+  }
+];
